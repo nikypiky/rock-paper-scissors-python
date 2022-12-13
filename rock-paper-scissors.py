3e3b1995c8109a90009fdec987b84adc9c5642b6
Niky Piky <naldorsc2@gmail.com>
2022-12-13 20:59:31 +0100

    finishing details + description of code

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -7,15 +7,17 @@ computer_points = 0
 
 while user_points < 5 and computer_points < 5:
 
-
+#user choice
     user_choice = input('Please select rock/paper/scissors: ').lower()
     if user_choice not in (choices):
         print('Please choose again')
         continue
 
+#computer choice
     computer_choice = random.choice(choices)
-    print(computer_choice)
+    print('Computer chose: ', computer_choice)
 
+#comparing user nad computer choices + counting score
     if user_choice == computer_choice:
         print('It\'s a tie!')
     elif (user_choice == 'rock' and computer_choice == 'paper') or (user_choice == 'paper' and computer_choice == 'scissors') or (user_choice == 'scissors' and computer_choice == 'rocke'):
@@ -25,12 +27,11 @@ while user_points < 5 and computer_points < 5:
         print('You win!!')
         user_points = user_points + 1
 
+#printing score after each round
     print('The score is: ' + str(user_points) + ' : ' + str(computer_points))
-    
-    print(user_points, computer_points)
+    print()
 
-print(user_points, computer_points)
-
+#anouncing final score
 if user_points > computer_points:
     print('You win!!')
 else:
